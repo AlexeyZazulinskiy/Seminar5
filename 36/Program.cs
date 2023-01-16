051e1bcc0ff14016a2edf7ab95979a5256d9649c
Alexey <alex3122007@yandex.ru>
2023-01-16 23:59:44 +0300

    DZ

---
+++ 36/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+void InputArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+
+int Release(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i= i+2){
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+Console.Clear();
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($" = {Release(array)}");
+
---
+++ 38/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+void InputArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+
+int Release(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    Console.WriteLine($" max {max}, min {min}");
+    return (max -Math.Abs(min));
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+Console.Clear();
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($" = {Release(array)}");
